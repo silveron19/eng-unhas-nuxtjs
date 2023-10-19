56b1ef9c104255b5db2bf74db81a1bd41e783858
Javier Jauhari <138309025+skadex17@users.noreply.github.com>
2023-10-19 18:35:55 +0800

    Added Welcome Components

--- app.vue
+++ app.vue
@@ -1,7 +1,6 @@
 import Header from './components/header.vue';
 <template>
-  <div class="bg-slate-900 min-h-screen w-screen text-white">
-    <Header />
-    <NuxtPage />
+  <div>
+    <welcome />
   </div>
 </template>
---
+++ components/welcome.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex justify-center items-center w-screen min-h-screen">
+        <div class="flex w-[66vw] h-[57vh] bg-[#063E63] flex-row z-[2]">
+            <div class="flex flex-col basis-2/3 p-20 text-[white]">
+                <div>
+                    <p class="text-[1rem] font-medium">SAMBUTAN DEKAN</p>
+                    <p class="text-[2rem] mt-[0.7vh] font-semibold">Prof. Dr. Eng. Ir. Muhammad Isran Ramli, S.T., M.T., IPM., ASEAN Eng.</p>
+                    <hr class="w-[15vw] h-[0.3vh] bg-[yellow] mt-[1vh]">
+                    <p class="text-[1.5rem] mt-[4vh]">Kami akan terus meningkatkan pencapaian menuju terwujudnya visi dan misi yang telah dicanangkan dalam rencana strategis fakultas.</p>
+                </div>
+                <p class="flex text-[1rem] text-[#CDB646] justify-end items-end translate-y-10 underline-offset-6">BACA SELENGKAPNYA</p>
+            </div>
+            <div class="flex basis-1/3 items-end justify-end">
+                <img src="../images/Rektor.png"/>
+            </div>
+        </div>   
+        <div class="absolute left-0 bottom-[14vh] w-[42vw] h-[17vh] bg-[#DF3F3F] z-[1]">
+        </div>   
+        <div class="flex justify-center items-center absolute left-20 -top-24 bottom-[14vh] w-[42vw] h-[39vh] bg-[#CDB646] z-[0]">
+            <p class="text-[2rem] text-center font-bold mb-10">SELAMAT DATANG DI FAKULTAS TEKNIK UNIVERSITAS HASANUDDIN</p>
+        </div> 
+    </div>
+    
+</template>
