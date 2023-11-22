74e4badff486cb290ae23150338bca3cfb3424bd
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-11-22 17:11:09 +0800

    fetching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,47 @@
 export default {
-  head: {
-    title: 'eng-unhas',
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content:
-          'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+  routeRules: {
+    "/testing": {
+      proxy: { to: "https://www.google.com/?hl=ID" },
+    },
+    "/berita": {
+      proxy: { to: "http://localhost:8055/items/berita" },
+    },
+    "/events": {
+      proxy: { to: "http://localhost:8055/items/events" },
+    },
+    "/galeri": {
+      proxy: { to: "http://localhost:8055/items/galeri" },
+    },
+    "/test": {
+      proxy: {
+        to: "http://localhost:8055/assets",
       },
-      { hid: 'description', name: 'description', content: 'eng-unhas' },
+    },
+  },
+  head: {
+    title: "eng-unhas",
+    meta: [
+      { charset: "utf-8" },
+      {
+        name: "viewport",
+        content:
+          "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+      },
+      { hid: "description", name: "description", content: "eng-unhas" },
     ],
   },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   modules: [
+<<<<<<< Updated upstream
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
     'nuxt-directus',
+=======
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+>>>>>>> Stashed changes
     [
-      '@nuxtjs/google-fonts',
+      "@nuxtjs/google-fonts",
       {
         families: {
           Rubik: true,
