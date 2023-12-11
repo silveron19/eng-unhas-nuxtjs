1bdbe8927eddefdc0dc6c7ec8092508fe97dccd3
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-12-11 13:43:30 +0800

    Fetching dan pengambilan data pada halaman visi misi

--- pages/profile/visi.vue
+++ pages/profile/visi.vue
@@ -1,3 +1,47 @@
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return { visi: [] }
+    },
+    methods: {
+        async fetchData() {
+            try {
+                const LINK = "http://localhost:8055/items/visi_misi/1"
+                const response = await axios.get(LINK)
+                let visi_misi = response.data.data
+                console.log(visi_misi['misi'].split('\n'))
+                this.visi = visi_misi
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    },
+    mounted() {
+        this.fetchData()
+    }
+
+}
+</script>
 <template>
-    <h1 class="text-black">Halaman Visi Misi dan Sasaran</h1>
+    <div class="text-black px-10 md:px-14 lg:px-16 xl:px-24 py-8 flex justify-center flex-col">
+        <h1 v-if="visi.length == 0" class="text-black">Halaman Visi Misi dan Sasaran</h1>
+        <div class="text-black" v-else>
+            <div class="my-4">
+                <div v-for="element in visi['konten'].split('\n')">
+                    <div :class="element.includes('\h3') ? 'text-2xl' : null" v-html="element"></div>
+                </div>
+            </div>
+            <div class="my-4">
+                <div v-for="element in visi['misi'].split('\n')">
+                    <div :class="element.includes('\h3') ? 'text-2xl' : null" v-html="element"></div>
+                </div>
+            </div>
+            <div class="my-4">
+                <div v-for="element in visi['sasaran'].split('\n')">
+                    <div :class="element.includes('\h3') ? 'text-2xl' : null" v-html="element"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
