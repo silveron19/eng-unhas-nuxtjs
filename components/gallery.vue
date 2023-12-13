0604adef87cd12d8ebb705bc8593911dfd1c0c5d
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-12-13 22:42:09 +0800

    Update

--- components/gallery.vue
+++ components/gallery.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from 'axios';
 import 'vue3-carousel/dist/carousel.css';
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 
@@ -14,54 +15,30 @@ export default {
     return {
       isMediumSize: this.checkScreenSize,
       slides: [
-        {
-          id: 1,
-          image: '_nuxt/images/gallery/image 1.jpg',
-          title: 'Title',
-        },
-        {
-          id: 2,
-          image: '_nuxt/images/gallery/image 2.jpg',
-          title: 'Title',
-        },
-        {
-          id: 3,
-          image: '_nuxt/images/gallery/image 3.jpg',
-          title: 'Title',
-        },
-        {
-          id: 4,
-          image: '_nuxt/images/gallery/image 1.jpg',
-          title: 'Title',
-        },
-        {
-          id: 5,
-          image: '_nuxt/images/gallery/image 2.jpg',
-          title: 'Title',
-        },
-        {
-          id: 6,
-          image: '_nuxt/images/gallery/image 3.jpg',
-          title: 'Title',
-        },
-        {
-          id: 7,
-          image: '_nuxt/images/gallery/image 3.jpg',
-          title: 'Title',
-        },
-        {
-          id: 8,
-          image: '_nuxt/images/gallery/image 3.jpg',
-          title: 'Title',
-        },
-        {
-          id: 9,
-          image: '_nuxt/images/gallery/image 3.jpg',
-          title: 'Title',
-        },
+
       ],
     };
   },
+  methods: {
+    checkScreenSize() {
+      return window.innerWidth >= 768;
+    },
+
+    async fetchData() {
+      const ENDPOINT = "http://localhost:8055/items/Galeries"
+      try {
+        const res = await axios.get(ENDPOINT)
+        this.slides = res.data.data
+        console.log(this.slides)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  },
+
+  mounted() {
+    this.fetchData()
+  }
 };
 </script>
 
@@ -98,6 +75,7 @@ export default {
 }
 
 @media screen and (min-width: 768px) {
+
   .carousel__prev,
   .carousel__next {
     top: 40%;
@@ -118,12 +96,10 @@ export default {
   <div id="galeri" class="bg-white py-8 px-10 md:px-14 lg:px-16 xl:px-24">
     <div class="flex flex-row mb-10 justify-between">
       <div class="w-1/2 flex flex-row items-center">
-        <h1
-          class="text-black text-lg md:text-xl lg:text-3xl font-bold align-middle">
+        <h1 class="text-black text-lg md:text-xl lg:text-3xl font-bold align-middle">
           GALERI
         </h1>
-        <div
-          class="ml-4 h-[4px] w-[75px] md:w-[250px] lg:w-[350px] xl:w-[400px] bg-yellow-600"></div>
+        <div class="ml-4 h-[4px] w-[75px] md:w-[250px] lg:w-[350px] xl:w-[400px] bg-yellow-600"></div>
       </div>
       <div class="more_button items-center p-2">
         <div class="flex items-center m-1 justify-between">
@@ -137,18 +113,15 @@ export default {
         <div class="w-full bg-[#063E63] h-[2px]"></div>
       </div>
     </div>
-    <carousel
-      :autoplay="2000"
-      :wrap-around="true"
-      :items-to-show="3"
-      :items-to-scroll="1">
+    <div v-if="slides.length == 0">
+      <h1>Loading</h1>
+    </div>
+    <carousel v-else :autoplay="2000" :wrap-around="true" :items-to-show="3" :items-to-scroll="1">
       <slide v-for="slide in slides" :key="slide.id">
         <div class="flex flex-col w-full">
-          <img
-            :src="slide.image"
-            class="object-cover h-[100px] md:h-[150px] lg:h-[250px]"
-            alt="Slide {{ slide.id }}" />
-          <p class="text-black font-bold mt-2">{{ slide.title }}</p>
+          <img :src="`http://localhost:8055/assets/${slide.image}`"
+            class="object-cover h-[100px] md:h-[150px] lg:h-[250px]" alt="Slide {{ slide.id }}" />
+          <p class="text-black font-bold mt-2 line-clamp-1">{{ slide.title }}</p>
         </div>
       </slide>
       <template #addons>
